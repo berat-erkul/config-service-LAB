eb1b94db3d73c67e58c9e702a7888b99ebb83dee
beraterkul <beraterkul00@gmail.com>
2026-03-14 09:28:42 +0300

    feat: initialize Config Server with Spring Cloud Config and Vault integration

--- src/main/java/configservice/configservice/ConfigServiceApplication.java
+++ src/main/java/configservice/configservice/ConfigServiceApplication.java
@@ -1,9 +1,11 @@
-package com.example.configservice;
+package configservice.configservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class ConfigServiceApplication {
 
     public static void main(String[] args) {
